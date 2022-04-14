208b25737656681f050ba9ecee96a70ab6c364f6
Joseph Bond <J.Bond@emc.com>
2022-04-14 09:04:18 -0400

    use current working dir

--- Pipelines/audit_pipeline.groovy
+++ Pipelines/audit_pipeline.groovy
@@ -38,7 +38,7 @@ pipeline {
       stage('audit pipelines') {
         	steps {
             	dir ('network_auto') {
-                  sh("echo 'hostname,running_version,netbox_version' > /tmp/switch_audit.csv")
+                  sh("echo 'hostname,running_version,netbox_version' > switch_audit.csv")
                 	sh('ansible-playbook -i netbox_inventory.yaml Pipelines/playbooks/check_switch_version.yaml')
             	}
         	}
