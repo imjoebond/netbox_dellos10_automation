77e51e8ddc06493b6eed24cb28a44a0c6e780cf3
Joe Bond <joseph.bond@dell.com>
2022-05-03 16:33:49 -0400

    fi x tpyo

--- Pipelines/audit_pipeline.groovy
+++ Pipelines/audit_pipeline.groovy
@@ -36,7 +36,7 @@ pipeline {
         dir ('network_auto') {
           sh("echo 'hostname,running_version,netbox_version' > Pipelines/playbooks/switch_audit.csv")
           sh('ansible-playbook -i netbox_inventory.yaml Pipelines/playbooks/check_switch_version.yaml')
-          archiveArtifacts artifacts: 'Pipeline/playbooks/switch_audit.csv', followSymlinks: false
+          archiveArtifacts artifacts: 'Pipelines/playbooks/switch_audit.csv', followSymlinks: false
         }
       }
       }
